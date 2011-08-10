0310f85910f3de1d912d58db7c47a309c0a79edc
oleksandr dodatko <dodikk88.reg@gmail.com>
2011-08-10 14:48:26 +0300

    Added graffiti GUI library and bundle.

---
+++ lib/GraffitiUI/GraffitiUI/GUGraffitiView.h
@@ -0,0 +1,16 @@
+//
+//  GUGraffitiView.h
+//  GraffitiUI
+//
+//  Created by Oleksandr Dodatko on 8/10/11.
+//  Copyright 2011 EPAM systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface GUGraffitiView : UIView {
+
+}
+
+@end
---
+++ lib/GraffitiUI/src/GUGraffitiView.m
@@ -0,0 +1,37 @@
+//
+//  GUGraffitiView.m
+//  GraffitiUI
+//
+//  Created by Oleksandr Dodatko on 8/10/11.
+//  Copyright 2011 EPAM systems. All rights reserved.
+//
+
+#import "GUGraffitiView.h"
+
+
+@implementation GUGraffitiView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
